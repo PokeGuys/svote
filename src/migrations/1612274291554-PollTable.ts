00243eb33f5bdb86426ece7ad5d1c8d3feeb7be7
PokeGuy <5060799+PokeGuys@users.noreply.github.com>
2021-02-03 20:04:53 +0800

    chore: added poll table migration scripts.

---
+++ src/migrations/1612274291554-PollTable.ts
@@ -0,0 +1,76 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
+
+export class PollTable1612274291554 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'polls',
+        columns: [
+          {
+            name: 'pollId',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'creatorId',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'count',
+            type: 'int',
+            unsigned: true,
+            default: 0,
+          },
+          {
+            name: 'isActive',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'startAt',
+            type: 'DATE',
+          },
+          {
+            name: 'endAt',
+            type: 'DATE',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.createIndices('polls', [
+      new TableIndex({
+        columnNames: ['startAt'],
+      }),
+      new TableIndex({
+        columnNames: ['endAt'],
+      }),
+      new TableIndex({
+        columnNames: ['isActive'],
+      }),
+    ]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query('DROP TABLE polls');
+  }
+}
