4c8addf04154640b2e940794d29ab36179e60707
PokeGuy <5060799+PokeGuys@users.noreply.github.com>
2021-02-05 11:58:15 +0800

    feat: add exchange refresh token API.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthPayloadDto } from './dto/auth-payload.dto';
 import { AuthDto } from './dto/auth.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -17,7 +18,25 @@ export class AuthController {
     description: 'Return JWT access token with an hour expiration.',
   })
   public async authenticate(@Body() authDto: AuthDto) {
-    const token = await this.authService.createToken(authDto.hkid);
-    return new AuthPayloadDto(token);
+    const { hkid } = authDto;
+    const token = await this.authService.createToken(hkid);
+    const refreshToken = await this.authService.createRefreshToken(hkid);
+    return new AuthPayloadDto(token, refreshToken);
+  }
+
+  @Post('refresh')
+  @ApiOperation({ summary: 'Exchange a refresh token for an access token' })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    type: AuthPayloadDto,
+    description: 'Return new access token and refresh token.',
+  })
+  @UseGuards()
+  public async refreshToken(@Body() refreshDto: RefreshTokenDto) {
+    const { refreshToken } = refreshDto;
+    const { token, refreshToken: newRefreshToken } = await this.authService.createAccessTokenFromRefreshToken(
+      refreshToken,
+    );
+    return new AuthPayloadDto(token, newRefreshToken);
   }
 }
--- src/auth/dto/auth-payload.dto.ts
+++ src/auth/dto/auth-payload.dto.ts
@@ -2,11 +2,17 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthPayloadDto {
   @ApiProperty({
-    description: 'JWT token',
+    description: 'JWT token (default: expired in an hour)',
   })
   token: string;
 
-  constructor(token: string) {
+  @ApiProperty({
+    description: 'JWT refresh token (default: expired in 7days)',
+  })
+  refreshToken: string;
+
+  constructor(token: string, refreshToken: string) {
     this.token = token;
+    this.refreshToken = refreshToken;
   }
 }
